15302b237a62fe7b2d242dee630d6b780b7f7d90
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-12-31 16:37:40 +0200

    Added some mock data

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ import postRoutes from "./routes/post.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/post.js";
 import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -64,6 +67,10 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+
+    /* MANUALLY ADD DATA ONCE!!! */ 
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => {
     console.log(`${error} did not connect`);
